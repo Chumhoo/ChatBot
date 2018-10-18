4f21c953acf383dbce254af7145bfffda12f1b74
Zhihao Li <515646122@qq.com>
2018-10-17 23:35:07 -0400

    Initial Commit

---
+++ js/index.js
@@ -0,0 +1,131 @@
+(function(){
+  
+  var chat = {
+    messageToSend: '',
+    messageResponses: [
+      'Why did the web developer leave the restaurant? Because of the table layout.',
+      'How do you comfort a JavaScript bug? You console it.',
+      'An SQL query enters a bar, approaches two tables and asks: "May I join you?"',
+      'What is the most used language in programming? Profanity.',
+      'What is the object-oriented way to become wealthy? Inheritance.',
+      'An SEO expert walks into a bar, bars, pub, tavern, public house, Irish pub, drinks, beer, alcohol'
+    ],
+    init: function() {
+      this.cacheDOM();
+      this.bindEvents();
+      this.render();
+    },
+    cacheDOM: function() {
+      this.$chatHistory = $('.chat-history');
+      this.$button = $('button');
+      this.$textarea = $('#message-to-send');
+      this.$chatHistoryList =  this.$chatHistory.find('ul');
+    },
+    bindEvents: function() {
+      this.$button.on('click', this.addMessage.bind(this));
+      this.$textarea.on('keyup', this.addMessageEnter.bind(this));
+    },
+    render: function(message) {
+      this.scrollToBottom();
+      if (this.messageToSend.trim() !== '') {
+        var template = Handlebars.compile( $("#message-template").html());
+        var context = { 
+          messageOutput: this.messageToSend,
+          time: this.getCurrentTime()
+        };
+
+        this.$chatHistoryList.append(template(context));
+        this.scrollToBottom();
+        this.$textarea.val('');
+        
+        // responses
+        var templateResponse = Handlebars.compile( $("#message-response-template").html());
+        var contextResponse = { 
+          response: message, //this.getRandomItem(this.messageResponses),
+          time: this.getCurrentTime()
+        };
+        
+        setTimeout(function() {
+          this.$chatHistoryList.append(templateResponse(contextResponse));
+          this.scrollToBottom();
+        }.bind(this), 1500);
+        
+      }
+      
+    },
+    
+    addMessage: function() {
+      dialog = this        
+      dialog.messageToSend = dialog.$textarea.val() 
+
+      ///////////////// /The API //////////////////////
+      var apigClient = apigClientFactory.newClient({
+        accessKey: 'AKIAIJDWEWH7XX7U4SLQ',
+        secretKey: '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',
+        region: 'eu-east-2',
+        apiKey: 'TPkkvfAbSABeVKyfWcJAam98sMoPYz8a2axBV3Gb'
+      });
+      var params = { };
+      var body = {
+        messages: [
+          {
+              type: "string",
+              unstructured: {
+                  id: "0",
+                  text: dialog.messageToSend,
+                  timestamp: "string"
+              }
+          }
+      ]
+      };
+      var additionalParams = {};
+      
+      apigClient.chatbotPost(params, body, additionalParams)
+          .then(function(result){
+            dialog.render(result.data.messages[0].unstructured.text);
+              //This is where you would put a success callback
+          }).catch( function(result){
+              //This is where you would put an error callback
+          });
+      ////////////////// End of the API //////////////////////
+    },
+    addMessageEnter: function(event) {
+        // enter was pressed
+        if (event.keyCode === 13) {
+          this.addMessage();
+        }
+    },
+    scrollToBottom: function() {
+       this.$chatHistory.scrollTop(this.$chatHistory[0].scrollHeight);
+    },
+    getCurrentTime: function() {
+      return new Date().toLocaleTimeString().
+              replace(/([\d]+:[\d]{2})(:[\d]{2})(.*)/, "$1$3");
+    },
+    getRandomItem: function(arr) {
+      return arr[Math.floor(Math.random()*arr.length)];
+    }
+    
+  };
+  
+  chat.init();
+  
+  var searchFilter = {
+    options: { valueNames: ['name'] },
+    init: function() {
+      var userList = new List('people-list', this.options);
+      var noItems = $('<li id="no-items-found">No items found</li>');
+      
+      userList.on('updated', function(list) {
+        if (list.matchingItems.length === 0) {
+          $(list.list).append(noItems);
+        } else {
+          noItems.detach();
+        }
+      });
+    }
+  };
+  
+  searchFilter.init();
+  
+})();
